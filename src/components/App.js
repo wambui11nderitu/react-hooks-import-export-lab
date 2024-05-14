9a654d7315dce0f547a1c27f0877ee1e7e867bf3
Wambui <nderituwambui93@gmail.com>
2024-05-14 15:14:30 +0300

    updating labs

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,17 @@
 import React from "react";
+import About from "./About";
+import Home from "./Home";
+import NavBar from "./NavBar";
+
 
 function App() {
   return (
-    <div>
-      <NavBar />
-      <Home />
-      <About />
-    </div>
+   <div>
+  <NavBar />
+  <Home />
+  <About />
+</div>
+
   );
 }
 
